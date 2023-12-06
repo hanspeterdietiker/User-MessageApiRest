4fcab2ba4850ae34297b1ca2b6fee7ee23e28627
Hanspeter <hansdietiker2012@gmail.com>
2023-12-06 14:28:11 -0300

    Code/v1 fixing/Updating MessageService/MessageController

--- MessageApi/Controllers/MessageController.cs
+++ MessageApi/Controllers/MessageController.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MessageAPI.Interfaces;
+using MessageAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MessageAPI.Controllers
 {
-    [Route("api/[controller]")]
+        /**
+         * updating code with UserService
+         * fixing the code
+         */
     [ApiController]
+    [Route("api/[controller]")]
     public class MessageController : ControllerBase
     {
+        private readonly IMessageService _messageService;
+        private readonly IUserService _userService;
+
+        public MessageController(IMessageService messageService, IUserService userService)
+        {
+        
+            _messageService = messageService;
+        }
+
+        [HttpGet("searching-message/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var msgFound = await _messageService.FindByIdAsync(id);
+            return Ok(msgFound);
+        }
+        [HttpPost("creating-message")]
+        /*[ValidateAntiForgeryToken]*/
+        public async Task<IActionResult> CreateMessage(MessageModel message)
+        {
+            await _messageService.CreateMessageAsync(message);
+            return Created("Message Created at Data Base", message);
+        }
+
+        [HttpPut("updating-message")]
+        /*[ValidateAntiForgeryToken]*/
+        public async Task<IActionResult> UpdateMessage(Guid id, [FromBody] MessageModel message)
+        {
+            await _messageService.UpdateAsync(id, message);
+            return Ok(message);
+        }
+        [HttpDelete("deleting-message/{id}")]
+        public IActionResult RemoveMessage(Guid id)
+        {
+            _messageService.RemoveMessageAsync(id);
+            return Ok("Message Deleted");
+        }
     }
 }
